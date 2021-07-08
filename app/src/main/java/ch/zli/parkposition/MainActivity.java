e2dd7739eef4530947ced58f3556b2af22ef1644
Nirushaan <nirush2002@gmail.com>
2021-07-08 15:52:13 +0200

    init 15:51

--- app/src/main/java/ch/zli/parkposition/MainActivity.java
+++ app/src/main/java/ch/zli/parkposition/MainActivity.java
@@ -24,6 +24,8 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
@@ -40,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient client;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,18 +53,18 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView_foto);
 
 
-
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                            .findFragmentById(R.id.google_map);
+                .findFragmentById(R.id.google_map);
 
         client = LocationServices.getFusedLocationProviderClient(this);
 
         if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
-
-
 
 
         button_foto.setOnClickListener(new View.OnClickListener() {
@@ -88,7 +89,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void getCurrentLocation(){
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         Task<Location> task = client.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
@@ -97,12 +108,17 @@ public class MainActivity extends AppCompatActivity {
                     supportMapFragment.getMapAsync(new OnMapReadyCallback() {
                         @Override
                         public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
-
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng)
+                                                    .title("I'm there");
+                            googleMap.addMarker(options);
                         }
                     });
                 }
             }
-        })
+        });
     }
+
+
 
 }
