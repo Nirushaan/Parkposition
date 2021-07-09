81fd20e71f741f69da7a0f4517b43e63325c5160
Nirushaan <nirush2002@gmail.com>
2021-07-09 14:32:15 +0200

    Get location latitude & longitude

--- app/src/main/java/ch/zli/parkposition/MainActivity.java
+++ app/src/main/java/ch/zli/parkposition/MainActivity.java
@@ -1,25 +1,41 @@
 package ch.zli.parkposition;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     Button button_foto;
     Button button_position;
     Button button_reset;
     ImageView imageView;
+
+    LocationManager locationManager;
+
+    ByteArrayOutputStream baos;
 
     double latitude,longitude;
 
@@ -35,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView_foto);
 
 
+
+
         Fragment fragment = new MapActivity();
 
 
@@ -47,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         button_position.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MarkerOptions markerOptions;
+                getLocation();
             }
         });
 
@@ -59,15 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 100);
             }
         });
-
-
-        button_position.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
 
         button_reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,7 +96,71 @@ public class MainActivity extends AppCompatActivity {
             Bitmap captureImage = (Bitmap) data.getExtras().get("data");
             //Set Capture Image to ImageView
 
+
+
             imageView.setImageBitmap(captureImage);
         }
+    }
+
+
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Called when the location has changed.
+     *
+     * @param location the updated location
+     */
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+
+        System.out.println(latitude + " : " + longitude);
+
+    }
+
+    /**
+     * This callback will never be invoked on Android Q and above, and providers can be considered
+     * as always in the {@link LocationProvider#AVAILABLE} state.
+     *
+     * @param provider
+     * @param status
+     * @param extras
+     * @deprecated This callback will never be invoked on Android Q and above.
+     */
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    /**
+     * Called when the provider is enabled by the user.
+     *
+     * @param provider the name of the location provider that has become enabled
+     */
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Called when the provider is disabled by the user. If requestLocationUpdates
+     * is called on an already disabled provider, this method is called
+     * immediately.
+     *
+     * @param provider the name of the location provider that has become disabled
+     */
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
     }
 }
--- app/src/main/java/ch/zli/parkposition/MapActivity.java
+++ app/src/main/java/ch/zli/parkposition/MapActivity.java
@@ -1,39 +1,61 @@
 package ch.zli.parkposition;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.jetbrains.annotations.NotNull;
+
+import static androidx.core.content.ContextCompat.getSystemService;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MapActivity#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapActivity extends Fragment{
+public class MapActivity extends Fragment {
 
     Button positionBtn;
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_map,container,false);
+
+
 
         SupportMapFragment supportMapFragment = (SupportMapFragment)getChildFragmentManager()
                 .findFragmentById(R.id.google_maps);
@@ -56,6 +78,10 @@ public class MapActivity extends Fragment{
                 });
             }
         });
+
+
+
+
         return view;
     }
 }
